ea75f7d0005d117294978c9bf89e46c3564c230d
borysandriychuk <andriychuk23@gmail.com>
2021-02-02 21:26:34 +0200

    owu html

--- lesson06/homeWork/figma/mobileFigma.js
+++
@@ -1,3 +0,0 @@
-document.querySelector('.button_mobile').onclick = function (){
-    document.querySelector('.header-nav-text').classList.toggle('active');
-}
--- lesson06/practice/js/main.js
+++
@@ -1,3 +0,0 @@
-document.querySelector('.button_mobile').onclick = function (){
-    document.querySelector('.header_nav').classList.toggle('active');
-}
